c0719005f610ea3b5fb1a38c2df12e81826484db
Micke <cruzmiguelangel858@gmail.com>
2024-05-02 11:51:41 -0600

    Hook useState

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,17 +1,22 @@
+import { useState } from 'react'; 
 import PropTypes from 'prop-types';
 export const CounterApp = ( {value} ) => {
   
-  const handleAdd = (event) => { 
-    console.log(event);
-  }
+  const [ counter, setCounter] = useState( value );
+  
+  const handleAdd = (event) => setCounter( (c) => c + 1);
+
+  const handleRemove = (event) => setCounter( (c) => c + -1);
+
+  const reset = (event) => setCounter( (c) => c = value);
 
   return (
     <>
       <h1>CounterApp</h1>
-      <h2> {value} </h2>
-      <button onClick = { handleAdd }>
-        +1
-      </button>
+      <h2> {counter} </h2>
+      <button onClick = { handleAdd }>+1</button>
+      <button onClick = { handleRemove}>-1</button>
+      <button onClick = { reset }>Reset</button>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,6 @@ import { CounterApp } from './CounterApp';
 //? Todo lo que se esta definiendo posterior a tittle son porperties
 ReactDOM.createRoot(document.getElementById('root' )).render(
   <React.StrictMode>
-    <CounterApp  value = {123}/>
+    <CounterApp  value = {0}/>
   </React.StrictMode>
 );
